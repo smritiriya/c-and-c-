3402d0b54f39b846ac0f8fd3326b2e28d59c2eca
smritiriya <68005580+smritiriya@users.noreply.github.com>
2020-07-08 15:18:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int a,b;
+ printf("enter two numbers");
+ scanf("%d %d",&a,&b);
+  printf("\n sum of%d and %d is %d",a,b,a+b);
+  printf("\n difference of %d and %d is %d",a,b,a-b);
+  printf("\n product of %d and %d is %d",a,b,a*b);
+  printf("\n division of %f and %f is %f",a,b,a/b);
+  printf("\n modulus of %d and %d is %d",a,b,a%b);
+
+
+    return 0;
+}
